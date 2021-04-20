fb8e0b4946469db1ab04b5eb54040f6f3d8f415d
Sergiomgp <sergiomgpinheiro@gmail.com>
2021-04-20 17:52:07 +0100

    - Re added ChargeAttack Visual Effects - Changed code on Hands prefab to work with the Effects

--- Assets/Scripts/Player/Animation/Left_Hand_AnimationController.cs
+++ Assets/Scripts/Player/Animation/Left_Hand_AnimationController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Left_Hand_AnimationController : MonoBehaviour
 {
+    PlayerAttack chargeAttack;
     private Animator anim;
-    private GameObject windEffect;
+    private GameObject petraEffect;
     private PlayerAttack LH_shoot;
+
+    ParticleSystem charge;
+
     // Start is called before the first frame update
     void Start()
     {
+        chargeAttack = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttack>();
         anim = GetComponent<Animator>();
         LH_shoot = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttack>();
-        windEffect = GameObject.FindGameObjectWithTag("WindParticle");
+        petraEffect = GameObject.FindGameObjectWithTag("PetraParticle");
+        charge = petraEffect.GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -22,15 +28,39 @@ public class Left_Hand_AnimationController : MonoBehaviour
         if (LH_shoot.leftHand)
         {
             anim.SetBool("IsPressed", true);
-            windEffect.SetActive(true);
+            petraEffect.SetActive(true);
+            ChargeEmissionRate();
             //Debug.Log("LH_SHOOT is now " + LH_shoot.leftHand);
         }
 
         if (!LH_shoot.leftHand)
         {
             anim.SetBool("IsPressed", false);
-            windEffect.SetActive(false);
+            petraEffect.SetActive(false);
             //Debug.Log("LH_SHOOT is now " + LH_shoot.leftHand);
+        }
+    }
+
+    public void ChargeEmissionRate()
+    {
+        var emission = charge.emission;
+        var disableEmisison = charge.emission;
+
+        if (chargeAttack.chargeTime < 0.7)
+        {
+            emission.rateOverTime = 0;
+        }
+        else if (chargeAttack.chargeTime < 1)
+        {
+            emission.rateOverTime = 5;
+        }
+        else if (chargeAttack.chargeTime < 2)
+        {
+            emission.rateOverTime = 7;
+        }
+        else
+        {
+            emission.rateOverTime = 15;
         }
     }
 }
--- Assets/Scripts/Player/Animation/Right_Hand_AnimationController.cs
+++ Assets/Scripts/Player/Animation/Right_Hand_AnimationController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Right_Hand_AnimationController : MonoBehaviour
 {
+    PlayerAttack chargeAttack;
     private Animator anim;
     private GameObject flameEffect;
+    private GameObject fullyCharged;
     private PlayerAttack RH_shoot;
+
+    ParticleSystem charge;
+
     // Start is called before the first frame update
+
     void Start()
     {
+        chargeAttack = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttack>();
         anim = GetComponent<Animator>();
         RH_shoot = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttack>();
         flameEffect = GameObject.FindGameObjectWithTag("FlameParticle");
+        charge = flameEffect.GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -23,12 +31,37 @@ public class Right_Hand_AnimationController : MonoBehaviour
         {
             anim.SetBool("IsPressed", true);
             flameEffect.SetActive(true);
+            ChargeEmissionRate();
+
         }
 
         if (!RH_shoot.rightHand)
         {
             anim.SetBool("IsPressed", false);
             flameEffect.SetActive(false);
+        }
+    }
+    //changes the emission rate of the particleSystem to give a better visual feedback when the attack is fully charged
+    public void ChargeEmissionRate()
+    {
+        var emission = charge.emission;
+        var disableEmisison = charge.emission;
+
+        if (chargeAttack.chargeTime < 0.7)
+        {
+            emission.rateOverTime = 0;
+        }
+        else if (chargeAttack.chargeTime < 1)
+        {
+            emission.rateOverTime = 5;
+        }
+        else if (chargeAttack.chargeTime < 2)
+        {
+            emission.rateOverTime = 7;
+        }
+        else
+        {
+            emission.rateOverTime = 12;
         }
     }
 }
