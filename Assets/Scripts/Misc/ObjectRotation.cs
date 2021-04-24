0708c6907803dcecf82c3d07eef469c5dbebd8c8
Sergiomgp <sergiomgpinheiro@gmail.com>
2021-04-24 17:56:12 +0100

    - fixes to some scene elements and trap prefab

--- Assets/Scripts/Misc/ObjectRotation.cs
+++ Assets/Scripts/Misc/ObjectRotation.cs
@@ -1,14 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ObjectRotation : MonoBehaviour
 {
-    public float xAngle, yAngle, zAngle;
-    public float rotationSpeed;
-    // Update is called once per frame
-    void Update()
+    // Instead of exposing 3 unrelated vars, use the Vector3 type
+    //public float xAngle, yAngle, zAngle;
+    public Vector3 rotationVelocity;
+    
+    // No need for rotation speed AND yAngle, as they represent almost the same information.
+    // Let's combine that into rotationVelocity instead.
+    //public float rotationSpeed;
+
+    // The gameObject has a RigidBody. Keep a reference for common usage.
+    public Rigidbody rigidBody = null;
+
+    private void Start()
     {
-        transform.Rotate(xAngle, yAngle * rotationSpeed * Time.deltaTime, zAngle,  Space.Self);
+        // Allow failure if not all refs are properly linked. Added a log to keep track of them.
+        if(rigidBody == null)
+        {
+            Debug.LogWarning($"ObjectRotation :: Start : Missing rigidbody reference in the object {gameObject.name}. Please repair.");
+            rigidBody = GetComponent<Rigidbody>();
+        }
+    }
+
+    // Never move or rotate during the Update phase because it invalidates the physics
+    // simulation and may need to reprocess parts of the simulation, which is bad.
+    // The physics engine has its own phase named "FixedUpdate" for modifying rigidbodies.
+    void FixedUpdate()
+    {
+        // Notice the new Time.fixedDeltaTime :)
+        Quaternion deltaRotation = Quaternion.Euler(rotationVelocity * Time.fixedDeltaTime);
+        rigidBody.MoveRotation(rigidBody.rotation * deltaRotation);
     }
 }
