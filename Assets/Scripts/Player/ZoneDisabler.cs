2ec8c428b9508a3ae62fd35c04bdc157046806bc
Sergiomgp <sergiomgpinheiro@gmail.com>
2021-05-30 20:03:10 +0100

    added - disabling rooms via script

---
+++ Assets/Scripts/Player/ZoneDisabler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneDisabler : MonoBehaviour
+{
+
+    [SerializeField] GameObject TrapSewer;
+    [SerializeField] GameObject PrisionRoom;
+    [SerializeField] GameObject SecretPassage;
+    [SerializeField] GameObject Room_2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Component enabled");
+            TrapSewer.SetActive(false);
+            PrisionRoom.SetActive(false);
+            SecretPassage.SetActive(false);
+            Room_2.SetActive(false);
+        }
+    }
+}
